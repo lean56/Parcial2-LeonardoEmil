807e640b70b4b145e70b9c9014326b1c63f9b9f1
leonardo emil <theflaco56@hotmail.com>
2019-06-25 14:41:20 -0400

    creando contexto

---
+++ Parcial2-LeonardoEmil/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_LeonardoEmil.DAL
+{
+   public class Contexto : DbContext
+    {
+        public DbSet<Asignatura>asignatura { get; set; }
+        public DbSet<Estudiante>estudiante { get; set; }
+        public DbSet<Inscripcion>inscripcion { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
