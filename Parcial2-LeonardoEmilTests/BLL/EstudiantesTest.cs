381a90ec4147d4fdea2ddf974a27e759d1d7a067
leonardo emil <theflaco56@hotmail.com>
2019-06-25 15:18:35 -0400

    creando estudiantesTest

---
+++ Parcial2-LeonardoEmilTests/BLL/EstudiantesTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_LeonardoEmil.BLL;
+using Parcial2_LeonardoEmil.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_LeonardoEmil.BLL.Tests
+{
+    [TestClass()]
+    public class EstudiantesTest
+    {
+
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Estudiantes> db = new RepositorioBase<Estudiantes>();
+
+            Estudiantes estudiante = new Estudiantes()
+            {
+                EstudianteId = 1,
+                FechaIngreso = DateTime.Now,
+                Nombres = "Juan Perez",
+                Balance = 1000
+            };
+
+            Assert.IsTrue(db.Guardar(estudiante));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Estudiantes> db = new RepositorioBase<Estudiantes>();
+
+            Estudiantes estudiante = new Estudiantes()
+            {
+                EstudianteId = 1,
+                FechaIngreso = DateTime.Now,
+                Nombres = "Juan Perez",
+                Balance = 10500
+            };
+
+            Assert.IsTrue(db.Modificar(estudiante));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Estudiantes> db = new RepositorioBase<Estudiantes>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Estudiantes> db = new RepositorioBase<Estudiantes>();
+
+            Assert.IsNotNull(db.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Estudiantes> db = new RepositorioBase<Estudiantes>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+
+    }
+}
