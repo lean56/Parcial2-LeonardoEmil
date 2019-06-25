0fb56501c9c517ae532de8bcc0e4b82a78b147d9
leonardo emil <theflaco56@hotmail.com>
2019-06-25 14:53:58 -0400

    creando test asignaturas

---
+++ Parcial2-LeonardoEmilTests/BLL/AsignaturasTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_LeonardoEmil.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_LeonardoEmil.BLL.Tests
+{
+    [TestClass()]
+    public class AsignaturasTest
+    {
+    
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+     
+    }
+}
