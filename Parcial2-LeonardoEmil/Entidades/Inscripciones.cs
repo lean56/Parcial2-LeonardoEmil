393fd7745e9d5575993dd3737e381200ea07f92f
leonardo emil <theflaco56@hotmail.com>
2019-06-25 15:41:13 -0400

    agregando inscripciondetalle

--- Parcial2-LeonardoEmil/Entidades/Inscripciones.cs
+++ Parcial2-LeonardoEmil/Entidades/Inscripciones.cs
@@ -14,17 +14,22 @@ namespace Parcial2_LeonardoEmil.Entidades
         public DateTime Fecha { get; set; }
         public decimal Monto { get; set; }
 
-        public Inscripciones(int inscripcionId, DateTime fecha, decimal monto)
+        public virtual List<InscripcionesDetalle> Detalles { get; set; }
+
+        public Inscripciones(int inscripcionId, DateTime fecha, decimal monto, List<InscripcionesDetalle> detalles)
         {
             InscripcionId = inscripcionId;
             Fecha = fecha;
             Monto = monto;
+            Detalles = detalles;
         }
+
         public Inscripciones()
         {
             InscripcionId = 0;
             Fecha = DateTime.Now;
             Monto = 0;
+            this.Detalles = new List<InscripcionesDetalle>();
         }
     }
 }
